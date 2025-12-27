03a9029b36f595a57bad7534509be38dd5e8b489
明 <34388791+mmm1h@users.noreply.github.com>
2025-12-27 16:44:47 +0800

    321

--- ImmortalWrt构建/uci-defaults.sh
+++ ImmortalWrt构建/uci-defaults.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
-# ==========================================================
-# 1. 网络接口配置 (Network)
-# ==========================================================
-
-# --- WAN 口 (静态IP, 关闭IPv6) ---
+# 1. Network
+# WAN static, IPv6 off
 if ! uci get network.wan >/dev/null 2>&1; then
     uci set network.wan=interface
 fi
@@ -14,40 +11,34 @@ uci set network.wan.ipaddr='192.168.9.3'
 uci set network.wan.gateway='192.168.9.1'
 uci set network.wan.broadcast='192.168.9.255'
 uci set network.wan.netmask='255.255.255.0'
-uci set network.wan.ipv6='0'           # 关闭 WAN IPv6
-uci set network.wan.delegate='0'       # 禁止分发 IPv6
+uci set network.wan.ipv6='0'
+uci set network.wan.delegate='0'
 
-# 处理 DNS
+# DNS
 uci -q del network.wan.dns
 uci add_list network.wan.dns='192.168.9.20'
 
-# --- LAN 口 (静态IP, 关闭IPv6) ---
+# LAN static, IPv6 off
 uci set network.lan.device='eth0'
 uci set network.lan.proto='static'
 uci set network.lan.ipaddr='192.168.8.1'
 uci set network.lan.netmask='255.255.255.0'
-uci set network.lan.ipv6='0'           # 关闭 LAN IPv6
+uci set network.lan.ipv6='0'
 uci set network.lan.delegate='0'
 
 uci commit network
 
-# ==========================================================
-# 2. DHCP 设置 (关闭 IPv6 DHCP 和 LAN DHCP 服务)
-# ==========================================================
+# 2. DHCP
+# LAN DHCP/IPv6 off
+uci set dhcp.lan.ignore='1'
+uci set dhcp.lan.ra='disabled'
+uci set dhcp.lan.dhcpv6='disabled'
 
-# --- LAN DHCP ---
-# 既然你要求“DHCP功能全部关闭”，通常指不提供 DHCP 服务
-uci set dhcp.lan.ignore='1'            # 忽略 LAN 接口 DHCP 请求 (关闭 DHCP 服务)
-uci set dhcp.lan.ra='disabled'         # 关闭 IPv6 路由通告
-uci set dhcp.lan.dhcpv6='disabled'     # 关闭 DHCPv6
-
-# --- WAN DHCP ---
-# WAN 口本身作为客户端，配置为 static 协议其实已经不使用 DHCP 了
-# 这里确保 WAN 不会产生 IPv6 相关交互
+# WAN IPv6 off
 uci set dhcp.wan.ra='disabled'
 uci set dhcp.wan.dhcpv6='disabled'
 
-# --- DNS 安全设置 (关闭 DNSSEC, 重绑定保护) ---
+# DNS options
 uci set dhcp.@dnsmasq[0].dnssec='0'
 uci set dhcp.@dnsmasq[0].dnssec_check_unsigned='0'
 uci set dhcp.@dnsmasq[0].rebind_protection='0'
@@ -55,63 +46,69 @@ uci set dhcp.@dnsmasq[0].dns_redirect='0'
 
 uci commit dhcp
 
-# ==========================================================
-# 3. 防火墙配置 (Firewall - 全开模式)
-# ==========================================================
-
-# --- 常规设置 ---
+# 3. Firewall (accept all)
 uci set firewall.@defaults[0].input='ACCEPT'
 uci set firewall.@defaults[0].output='ACCEPT'
 uci set firewall.@defaults[0].forward='ACCEPT'
-uci set firewall.@defaults[0].syn_flood='0'    # 关闭 SYN-flood 保护
-uci set firewall.@defaults[0].fullcone='0'     # 关闭 FullCone NAT
+uci set firewall.@defaults[0].syn_flood='0'
+uci set firewall.@defaults[0].fullcone='0'
 
-# --- 区域设置 (LAN & WAN 全接受) ---
-# 修改 lan 区域
+# LAN/WAN zones
 uci set firewall.@zone[0].input='ACCEPT'
 uci set firewall.@zone[0].output='ACCEPT'
 uci set firewall.@zone[0].forward='ACCEPT'
 
-# 修改 wan 区域 (通常是 index 1，但用名字更稳妥)
-# 查找名为 wan 的 zone 并设置
 wan_zone_index=$(uci show firewall | grep ".name='wan'" | cut -d '[' -f2 | cut -d ']' -f1)
 if [ -n "$wan_zone_index" ]; then
     uci set firewall.@zone[$wan_zone_index].input='ACCEPT'
     uci set firewall.@zone[$wan_zone_index].output='ACCEPT'
     uci set firewall.@zone[$wan_zone_index].forward='ACCEPT'
-    uci set firewall.@zone[$wan_zone_index].masq='1' # 保持 NAT 开启，否则无法上网
+    uci set firewall.@zone[$wan_zone_index].masq='1'
 fi
 
 uci commit firewall
 
-# ==========================================================
-# 4. 系统与账户 (System & SSH)
-# ==========================================================
-
+# 4. System/SSH
 uci set system.@system[0].hostname='ImmortalWrt'
 uci set system.@system[0].zonename='Asia/Shanghai'
 uci set system.@system[0].timezone='CST-8'
 uci commit system
 
-# 界面主题与语言
+# LuCI
 uci set luci.main.mediaurlbase='/luci-static/argon'
 uci set luci.main.lang='zh_cn'
 uci commit luci
 
-# 密码设置 (Root)
+# Root password
 cat << 'EOF' | passwd root
 QSi$xmx6#Rn$$&
 QSi$xmx6#Rn$$&
 EOF
 
-# SSH 修复与允许 Root 登录
-# 确保 root 用户的 shell 是存在的 (ash)，防止因缺少 bash 导致无法登录
+# Allow root SSH
 sed -i 's|/bin/bash|/bin/ash|g' /etc/passwd
 sed -i 's|/bin/zsh|/bin/ash|g' /etc/passwd
 
 uci set dropbear.@dropbear[0].PasswordAuth='on'
 uci set dropbear.@dropbear[0].RootPasswordAuth='on'
-uci set dropbear.@dropbear[0].Interface='lan'  # 仅允许 LAN 口登录 SSH (更安全)
+uci set dropbear.@dropbear[0].Interface='lan'
 uci commit dropbear
+
+# Stop mihomo
+if [ -f /etc/init.d/mihomo ]; then
+    /etc/init.d/mihomo disable
+    /etc/init.d/mihomo stop
+fi
+
+# OpenClash core
+mkdir -p /etc/openclash/core
+if [ -f /usr/bin/mihomo ]; then
+    cp -f /usr/bin/mihomo /etc/openclash/core/clash_meta
+    chmod +x /etc/openclash/core/clash_meta
+fi
+
+# Use Meta core
+uci set openclash.config.core_version='meta'
+uci commit openclash
 
 exit 0
